c651657d207ebe6b91a0aaf7016dcaa399bdc448
Dhruv Kumar <dhruvcharvi@gmail.com>
2025-07-13 00:05:19 -0400

    Edited Class Piece

--- piece.cc
+++ piece.cc
@@ -1,2 +1,17 @@
 #include "piece.h";
 
+std::string Piece::getColour() const {
+    return colour;
+} // Piece::getColour
+
+void Piece::setColour(std::string newColour) {
+    colour = newColour;
+} // Piece::setColour
+
+bool Piece::getHasMoved() const {
+    return hasMoved;
+} // Piece::getHasMoved
+
+void Piece::setHasMoved(bool b) {
+    hasMoved = b;
+} // Piece::setHasMoved
