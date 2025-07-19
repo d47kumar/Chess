5e8891f2c0eb29611d0277693c412835f8fe3ca9
Abhigyan Srivastava <103575458+AbhigyanSrivastava10@users.noreply.github.com>
2025-07-19 19:18:24 -0400

    edited bishop

--- bishop.cc
+++ bishop.cc
@@ -10,9 +10,27 @@ bool Bishop::isValidMove(Position movePosition, Board *board) const {
     int newRow = movePosition.getRow();
     int newColumn = movePosition.getColumn();
 
-    // Diagonal move
-    if (abs(newRow - row) == abs(newColumn - column)) {
-        return true;
+    int rowDiff = abs(newRow - row);
+    int columnDiff = abs(newColumn - column);
+
+    // Not a bishop move
+    if (rowDiff != columnDiff) {
+        return false;
     }
-    return false;
+
+    // Check for blocking pieces
+    int dRow = (newRow - row) / rowDiff;
+    int dCol = (newColumn - column) / columnDiff;
+    for (int i = 1; i < rowDiff; ++i) {
+        Position intermediate(row + i * dRow, column + i * dCol);
+        if (board->getPiece(intermediate) != nullptr) {
+            return false;
+        }
+    }
+
+    Piece* destPiece = board->getPiece(movePosition);
+    if (destPiece && destPiece->getColour() == getColour()) {
+        return false;
+    }
+    return true;
 } // Bishop::isValidMove
