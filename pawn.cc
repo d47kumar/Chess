625ff3607705a7180ecfc825de999c82374cc9d8
Abhigyan Srivastava <103575458+AbhigyanSrivastava10@users.noreply.github.com>
2025-07-19 19:39:48 -0400

    added comments on pawn and potential to-dos

--- pawn.cc
+++ pawn.cc
@@ -13,19 +13,22 @@ bool Pawn::isValidMove(Position movePosition, Board *board) const {
 
     int direction = (getColour() == "WHITE") ? -1 : 1;
 
+    // Forward move (1 square)
     if (newColumn == column && newRow == row + direction) {
         return board->getPiece(movePosition) == nullptr;
     }
-
+    
+    // Opening move (2 squares from the starting pos)
     if (!getHasMoved() && newColumn == column && newRow == row + 2 * direction) {
         return board->getPiece(movePosition) == nullptr && 
                board->getPiece(Position(row + direction, column)) == nullptr;
     }
 
+    // capture logic
     if (abs(newColumn - column) == 1 && newRow == row + direction) {
         Piece* target = board->getPiece(movePosition);
         return target != nullptr && target->getColour() != getColour();
     }
-
+    // TODO: en passant logic andf promotion piece check 
     return false;
 } // Pawn::isValidMove
